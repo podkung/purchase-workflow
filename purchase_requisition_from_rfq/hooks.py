fb281cb159a586ef490bf2cf51e686f4666d5872
ps-tubtim <pimolnat.s@outlook.com>
2019-10-28 13:16:55 +0700

    [14.0][ADD] purchase_requisition_from_rfq

---
+++ purchase_requisition_from_rfq/hooks.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo.addons.purchase_requisition.models.purchase import PurchaseOrder
+
+
+def post_load_hook():
+    def new_write(self, vals):
+        return super(PurchaseOrder, self).write(vals)
+
+    if not hasattr(PurchaseOrder, "write_original"):
+        PurchaseOrder.write_original = PurchaseOrder.write
+
+    PurchaseOrder.write = new_write
