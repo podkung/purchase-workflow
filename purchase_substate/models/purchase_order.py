1791476a7cc67ec662008170cd0c52d1a69a89c0
Kitti U <kittiu@gmail.com>
2020-10-02 15:27:08 +0700

    [13.0][ADD] purchase_substate

---
+++ purchase_substate/models/purchase_order.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Akretion
+# Copyright 2020 Ecosoft (<http://ecosoft.co.th>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class BaseSubstateType(models.Model):
+    _inherit = "base.substate.type"
+
+    model = fields.Selection(selection_add=[("purchase.order", "Purchase order")])
+
+
+class PurchaseOrder(models.Model):
+    _inherit = ["purchase.order", "base.substate.mixin"]
+    _name = "purchase.order"
+    _state_field = "state"
