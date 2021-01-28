37ba03870652020136c7adee67d28595f109724e
Prapassorn Sornkaew <prapassorn.s@prothaitechnology.com>
2021-01-28 16:05:36 +0000

    [MIG] purchase_work_acceptance_late_fines: Migration to 14.0

--- purchase_work_acceptance_late_fines/models/account_move.py
+++ purchase_work_acceptance_late_fines/models/account_move.py
@@ -24,17 +24,20 @@ class AccountMove(models.Model):
             ("state", "!=", "cancel"),
             ("id", "not in", used_wa_ids.ids),
         ]
+        if self._context.get("default_partner_id", False):
+            dom.append(("partner_id", "=", self._context.get("default_partner_id")))
         return dom
 
     @api.onchange("partner_id")
     def _onchange_partner_late_wa(self):
-        self.late_wa_id = False
-        self.invoice_line_ids = False
-        self.line_ids = False
-        if self.partner_id:
-            domain = self._domain_late_wa()
-            domain.append(("partner_id", "=", self.partner_id.id))
-            return {"domain": {"late_wa_id": domain}}
+        if self.late_wa_id:
+            self.late_wa_id = False
+            self.invoice_line_ids = False
+            self.line_ids = False
+            if self.partner_id:
+                self.with_context(
+                    default_partner_id=self.partner_id.id
+                )._domain_late_wa()
 
     def _prepare_move_wa_late(self, wa):
         move_dict = {
@@ -48,9 +51,9 @@ class AccountMove(models.Model):
     def _onchange_late_wa_id(self):
         """ Auto fill values from WA delivery late fines """
         move_line = self.env["account.move.line"]
-        self.line_ids = False
         late_wa = self.late_wa_id
-        if self.type == "out_invoice" and late_wa:
+        if self.move_type == "out_invoice" and late_wa:
+            self.line_ids = False
             move_dict = self._prepare_move_wa_late(late_wa)
             move_line_dict = late_wa._prepare_late_wa_in_account_move_line()
             move_line_dict.update(
